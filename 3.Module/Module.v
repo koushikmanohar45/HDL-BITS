d3b0e82d4f6eb35b39c84037455be70fa9776abc
KOUSHIK C M <koushikmanohar83@gmail.com>
2026-04-09 18:34:50 +0530

    Create Module.v

---
+++ 3.Module/Module.v
@@ -0,0 +1,6 @@
+//You may connect signals to the module by port name or port position. For extra practice, try both methods.
+
+module top_module ( input a, input b, output out );
+    mod_a ins1(.in1(a),.in2(b),.out(out));
+endmodule
+
