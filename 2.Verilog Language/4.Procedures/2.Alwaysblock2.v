0a16903b6dcd060a836a02a85eb6ab4a5a6f02a0
KOUSHIK C M <koushikmanohar83@gmail.com>
2026-04-13 22:32:08 +0530

    Create 2.Alwaysblock2.v

---
+++ 2.Verilog Language/4.Procedures/2.Alwaysblock2.v
@@ -0,0 +1,18 @@
+//Build an XOR gate three ways, using an assign statement, a combinational always block, and a clocked always block. Note that the clocked always block produces a different circuit from the other two: There is a flip-flop so the output is delayed.
+
+module top_module(
+    input clk,
+    input a,
+    input b,
+    output wire out_assign,
+    output reg out_always_comb,
+    output reg out_always_ff   );
+    always@(posedge clk)begin
+        out_always_ff<=a^b;
+    end
+    always@(*)begin
+        out_always_comb=a^b;
+    end
+    assign out_assign=a^b;
+
+endmodule
