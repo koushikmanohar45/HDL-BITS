34f0c0e17f9eb252edc94cb675b6e1e41a9eac17
KOUSHIK C M <koushikmanohar83@gmail.com>
2026-04-13 23:05:00 +0530

    Create 2.Exams\m2014 q4i.v

---
+++ 3.Circuits/1.Combinational Logic/1.Basic Gates/2.Exams\m2014 q4i.v
@@ -0,0 +1,18 @@
+/*
+Implement the following circuit:
+                        _________________out
+                       |
+                       |
+                   ____|____
+                     _____
+            1'b0      ___
+                       _
+*/
+
+module top_module (
+    output out);
+    supply0 gnd;
+    assign out=gnd;
+endmodule
+
+
