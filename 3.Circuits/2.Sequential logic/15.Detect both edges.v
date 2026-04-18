c574f620a5812bec5717daa1b6ad67bbcec584e1
KOUSHIK C M <koushikmanohar83@gmail.com>
2026-04-18 11:34:19 +0530

    Create 15.Detect both edges.v

---
+++ 3.Circuits/2.Sequential logic/15.Detect both edges.v
@@ -0,0 +1,16 @@
+//For each bit in an 8-bit vector, detect when the input signal changes from one clock cycle to the next (detect any edge). The output bit should be set the cycle after a 0 to 1 transition occurs.
+
+module top_module (
+    input clk,
+    input [7:0] in,
+    output [7:0] anyedge
+);
+    int i;
+    reg [7:0] in_p;
+    always @(posedge clk)begin
+        in_p<=in;
+            anyedge<=(in_p^in);
+
+    end
+
+endmodule
