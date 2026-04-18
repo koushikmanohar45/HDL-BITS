9a93619aff15426281eb63a7382349ac2bc4e21b
KOUSHIK C M <koushikmanohar83@gmail.com>
2026-04-18 08:36:16 +0530

    Create 07.D latch.v

---
+++ 3.Circuits/2.Sequential logic/07.D latch.v
@@ -0,0 +1,26 @@
+/*
+mplement the following circuit:
+
+     _____________
+----| D          Q|-------
+    |             |
+    |             |
+----|ENA          |
+    |_____________|
+
+Note that this is a latch, so a Quartus warning about having inferred a latch is expected.
+*/
+
+
+module top_module (
+    input d, 
+    input ena,
+    output q);
+    always @(*)begin
+        if(ena)
+            q=d;
+        else
+            q=q;
+    end
+
+endmodule
