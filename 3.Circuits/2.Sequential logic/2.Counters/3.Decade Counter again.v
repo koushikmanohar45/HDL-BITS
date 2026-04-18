e160bd5e75fdf75bfdeecc7727c4108396459534
KOUSHIK C M <koushikmanohar83@gmail.com>
2026-04-18 11:42:53 +0530

    Create 3.Decade Counter again.v

---
+++ 3.Circuits/2.Sequential logic/2.Counters/3.Decade Counter again.v
@@ -0,0 +1,14 @@
+//Make a decade counter that counts 1 through 10, inclusive. The reset input is synchronous, and should reset the counter to 1.
+
+module top_module (
+    input clk,
+    input reset,
+    output reg  [3:0] q);
+    always @(posedge clk)begin
+        if(reset|| q==4'd10)
+            q<=4'd1;
+        else
+            q<=q+1; 
+    end
+
+endmodule
