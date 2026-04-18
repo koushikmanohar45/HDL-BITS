8bf1f19a234256500183c00663cb071b154144a8
KOUSHIK C M <koushikmanohar83@gmail.com>
2026-04-18 08:47:55 +0530

    Create 08.DFF.v

---
+++ 3.Circuits/2.Sequential logic/08.DFF.v
@@ -0,0 +1,25 @@
+/*
+Implement the following circuit:
+     _____________
+----| D          Q|-------
+    |             |
+    |             |
+----|>clk         |
+    |_____AR______|
+          |
+          |
+*/
+
+module top_module (
+    input clk,
+    input d, 
+    input ar,   // asynchronous reset
+    output q);
+    always@(posedge clk or posedge ar)begin
+        if(ar)
+            q<=1'b0;
+        else
+            q<=d;
+    end
+
+endmodule
