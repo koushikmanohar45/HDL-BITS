2dc072b45cc3f58e7f28c7bff7b2fe94d9e4b607
KOUSHIK C M <koushikmanohar83@gmail.com>
2026-04-24 23:20:20 +0530

    Create 04.Simple FSM 2(Synchronous Reset).v

---
+++ 3.Circuits/2.Sequential logic/5.Finite State Machines/04.Simple FSM 2(Synchronous Reset).v
@@ -0,0 +1,54 @@
+/*
+This is a Moore state machine with two states, two inputs, and one output. Implement this state machine.
+
+This exercise is the same as fsm2, but using synchronous reset.
+                    reset
+                      |
+                      v
+               +-------------+      j = 1      +-------------+
+          +--->|     OFF     |---------------->|      ON     |---+
+          |    |   out = 0   |<----------------|   out = 1   |   |
+          |    +-------------+      k = 1      +-------------+   |
+          |           ^                               ^          |
+          |           |                               |          |
+          |         j = 0                           k = 0        |
+          +-----------+                               +----------+
+
+*/
+
+module top_module(
+    input clk,
+    input reset,    // Synchronous reset to OFF
+    input j,
+    input k,
+    output out); //  
+
+    parameter OFF=0,ON=1; 
+    reg state, next_state;
+    always @(*) begin    
+        case(state)
+            ON:begin
+                if(k)
+                    next_state=OFF;
+                else
+                    next_state=ON;
+              end
+            OFF:begin
+                if(j)
+                    next_state=ON;
+                else
+                    next_state=OFF;
+            end
+            default:next_state=OFF;
+        endcase
+    end
+
+   always @(posedge clk ) begin   
+        if(reset)
+            state<=OFF;
+        else
+            state<=next_state;
+    end
+    assign out=(state==OFF)?1'b0:1'b1;
+
+endmodule
