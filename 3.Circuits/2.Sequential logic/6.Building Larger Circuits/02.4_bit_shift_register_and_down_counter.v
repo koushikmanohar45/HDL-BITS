b010c9b2cfe8a6df416fc412b97dc887ed4eef36
KOUSHIK C M <koushikmanohar83@gmail.com>
2026-04-30 21:45:52 +0530

    Create 02.4_bit_shift_register_and_down_counter.v

---
+++ 3.Circuits/2.Sequential logic/6.Building Larger Circuits/02.4_bit_shift_register_and_down_counter.v
@@ -0,0 +1,23 @@
+/*
+This is the first component in a series of five exercises that builds a complex counter out of several smaller circuits. See the final exercise for the overall design.
+
+Build a four-bit shift register that also acts as a down counter. Data is shifted in most-significant-bit first when shift_ena is 1. The number currently in the shift register is decremented when count_ena is 1. Since the full system doesn't ever use shift_ena and count_ena together, it does not matter what your circuit does if both control inputs are 1 (This mainly means that it doesn't matter which case gets higher priority).
+*/
+
+module top_module (
+    input clk,
+    input shift_ena,
+    input count_ena,
+    input data,
+    output [3:0] q);
+    
+    always @(posedge clk)begin
+        case({shift_ena,count_ena})
+            2'b00:q<=q;
+            2'b01:q<=q-1;
+            2'b10:q<={q[2:0],data};
+            default:q<=q;
+        endcase
+    end
+
+endmodule
