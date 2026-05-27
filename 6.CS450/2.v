82cfe46cea632a4a3f44c0ad820f9e9d10b0a9fd
KOUSHIK C M <koushikmanohar83@gmail.com>
2026-05-27 21:40:27 +0530

    Create 2.v

---
+++ 6.CS450/2.v
@@ -0,0 +1,37 @@
+/*
+Build a two-bit saturating counter.
+
+The counter increments (up to a maximum of 3) when train_valid = 1 and train_taken = 1. It decrements (down to a minimum of 0) when train_valid = 1 and train_taken = 0. When not training (train_valid = 0), the counter keeps its value unchanged.
+
+areset is an asynchronous reset that resets the counter to weakly not-taken (2'b01). Output state[1:0] is the two-bit counter value.
+*/
+
+module top_module(
+    input clk,
+    input areset,
+    input train_valid,
+    input train_taken,
+    output [1:0] state
+);
+    always@(posedge clk, posedge areset)
+        begin
+            if(areset)
+                state <= 2'b01;
+            else
+                begin
+                    casex({train_valid, train_taken})
+                           2'b11: begin
+                               		state<=(state==2'd3)? 2'd3:state+2'd1;
+                           		  end
+                           2'b10: begin
+                               		state<=(state==2'd0)?2'd0:state-2'd1;
+                           		  end
+                           2'b0?: begin
+                               		state<=state;
+                           		  end
+                           default: state<=2'd1;
+                    endcase
+              	end
+       	end
+
+endmodule
